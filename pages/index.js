92b4a5e4901be3d8298075cc3b0ccf96f4843438
Uno Zero Code <57536442+unozerocode@users.noreply.github.com>
2019-12-20 12:45:29 -0600

    add test image

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import React, {Component} from "react";
+//import test_pattern from "/EBUTEST_HD_EDIT2_16-9_01.jpg";
 const Index = () => (
     <div>
         <p>
             Uno Zero Code
         </p>
+    <img src="/EBUTEST_HD_EDIT2_16-9_01.jpg" width="300"/>
     </div>
 );
 
